fe99cc2fff46460cf12c99195b4ace7049f2a635
Scruggs <kid.idioteque@gmail.com>
2018-03-20 13:27:11 -0700

    completes app mock-ups

--- components/bottomDisplay.jsx
+++ components/bottomDisplay.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Header from './header'
 import Description from './description'
 import CurrentTime from './currentTime'
+import Ticker from './ticker'
 
 class BottomDisplay extends React.Component {
     render() {
@@ -10,6 +11,7 @@ class BottomDisplay extends React.Component {
             <Header />
             <Description />
             <CurrentTime />
+            <Ticker />
         </div>
     }
 }
---
+++ components/ticker.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Ticker extends React.Component {
+    render() {
+        return <div id="ticker">
+            "IS THIS REALLY NEWS?" AKS COMMENTER | 5  MILLION RETWEENS IN 1 HOUR ALREA"
+        </div>
+    }
+}
+
+export default Ticker
