830f5b95337257caa1cc4873d0822962fe68837e
Scruggs <kid.idioteque@gmail.com>
2018-03-20 13:17:47 -0700

    introduces mock CurrentTime component

--- components/bottomDisplay.jsx
+++ components/bottomDisplay.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Header from './header'
 import Description from './description'
+import CurrentTime from './currentTime'
 
 class BottomDisplay extends React.Component {
     render() {
         return <div id="bottom_display">
             <Header />
             <Description />
+            <CurrentTime />
         </div>
     }
 }
---
+++ components/currentTime.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class CurrentTime extends React.Component {
+    render() {
+        return <div id="current_time">
+            20:02
+        </div>
+    }
+}
+
+export default CurrentTime
