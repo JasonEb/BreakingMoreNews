243cb3cc318aa5aec2a47cbaba5165f3587d2654
Scruggs <kid.idioteque@gmail.com>
2018-03-20 12:30:45 -0700

    introduces main Header

--- components/app.jsx
+++ components/app.jsx
@@ -5,12 +5,14 @@ import NewsApp from './newsApp'
 
 // testing
 import LiveBox from './liveBox'
+import Header from './header'
 
 const App = () => (
     <div>
         <Switch>
             <Route exact path="/" component={NewsApp} />
-            <Route exact path="/livebox" component={LiveBox} />
+            <Route path="/livebox" component={LiveBox} />
+            <Route path="/header" component={Header} />
         </Switch>
     </div>
 )
