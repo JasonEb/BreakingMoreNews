7833ddb55318683afd0cd11c9e087224471ecb29
Scruggs <kid.idioteque@gmail.com>
2018-03-20 12:45:09 -0700

    adds background image

---
+++ components/background.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Background extends React.Component {
+    render() {
+        return(
+            <div id="background">
+                <img src="../assets/breaking-news.png"/>
+            </div>
+        )
+    }
+}
+
+export default Background;
