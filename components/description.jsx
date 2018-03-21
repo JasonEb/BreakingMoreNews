fdc364c161bc0273efd0288b6151834e03f9a647
Scruggs <kid.idioteque@gmail.com>
2018-03-21 16:46:14 -0700

    makes description content-editable

--- components/description.jsx
+++ components/description.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react';
+import ContentEditable from 'react-simple-contenteditable';
 
 class Description extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = { text: "PROFESSIONAL E-GAMER IN RUINS" }
+        this.handleChange = this.handleChange.bind(this);
+    }
+   
+    handleChange (ev, value) {
+      this.setState({
+        text: value
+      })
+    }
+
     render() {
+        let {text} = this.state
+        
         return <div id="description">
-            SOMETHING WENT VIRAL ONLINE
+            <ContentEditable
+            html={this.state.text}
+            onChange={ this.handleChange }
+            contentEditable="plaintext-only"
+            />
         </div>
     }
 }
