153666ad6701d32a614dacee9c3f770cb2e777d1
Scruggs <kid.idioteque@gmail.com>
2018-03-21 16:30:52 -0700

    implements updating time display

--- components/currentTime.jsx
+++ components/currentTime.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
 class CurrentTime extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { date: new Date() }
+        this.tick = this.tick.bind(this)
+        this.tickId = 0
+    }
+    tick(){
+        this.setState({ date: new Date() })
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.tickId)
+    }
+    componentDidMount() {
+        this.tickId = setInterval( this.tick , 1000*60)
+    }
     render() {
+        let {date} = this.state
+        console.log(date.getSeconds())
+        let hour = date.getHours()
+        let minutes = date.getMinutes()
+        minutes = minutes < 10 ? "0" + minutes : minutes
+
         return <div id="current_time">
-            20:02
+            {hour}<span className="colon_blinker">:</span>{minutes}
         </div>
     }
 }
